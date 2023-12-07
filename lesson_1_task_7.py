3346fe1a6a10f4298f45cc69576150768840900a
AnastasiiaNazarova <151529706+AnastasiiaNazarova@users.noreply.github.com>
2023-12-07 15:03:22 +0400

    Add files via upload

---
+++ lesson_1_task_7.py
@@ -0,0 +1,24 @@
+def Num1():
+    print(1)
+def Num2():
+    print(2)
+def Num3():
+    print(3)
+def Num4():
+    print(4)
+def Num5():
+    print(5)
+def Num6():
+    print(6)
+def Num7():
+    print(7)
+def Num8():
+    print(8)
+def Num9():
+    print(9)
+def Num0():
+    print(0)
+
+per= [Num8(), Num8(), Num0(), Num0(), Num5(), Num5(), Num5(), Num3(), Num5(), Num3(), Num5()]
+
+print(per)
